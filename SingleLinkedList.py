3d23969be9aa2fa39606604025526aadebd56ed3
SHIVAM GOYAL <learnandcode6335@gmail.com>
2019-07-30 22:46:15 +0530

    Completed Methods body Parts

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -20,10 +20,20 @@ class ListOperations(object):
             temp_iter.nexval = Node(data)
 
     def insert_at_beg(self, node):
-        pass
+        temp_data = self.head
+        self.head = node
+        node.nexval = temp_data
 
     def insert_in_mid(self, comp_data, data):
-        pass
+        if self.head is None :
+            print("*********** No Data Available ***********")
+        else:
+            prev_ele = curr_ele = self.head
+            while curr_ele is not None:
+                if curr_ele.data == comp_data:
+                    break
+                prev_ele = curr_ele
+
 
     def traversing(self):
         temp = self.head
@@ -42,17 +52,18 @@ for i in range(int(input("Enter Number of Nodes : "))):
     print("Enter {0} node Data".format(i + 1))
     obj.append_list(input())
 response = input("Want to do Operations - y or n : ")
-if response.lower() == 'y':
+while response.lower() == 'y':
     res = input("Insertion or Deletion : i or d")
     if res == 'i':
         data = input("Enter Data : ")
         num = int(input("Press 1 - Insertion in Beg , Press 2 - Insertion in mid, Press 3 -Insertion in end : "))
         if num == 1:
-            obj.insert_at_beg(data)
+            obj.insert_at_beg(Node(data))
         elif num == 2:
             comp_data = input("Enter comparable element :")
-            obj.insert_in_mid(comp_data,data)
+            obj.insert_in_mid(comp_data,Node(data))
         else:
-            obj.append_list(data)
+            obj.append_list(Node(data))
+    response = input("Want to do more operations ? y or n")
 
 obj.traversing()
